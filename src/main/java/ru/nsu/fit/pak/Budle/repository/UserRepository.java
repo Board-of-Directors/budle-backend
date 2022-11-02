d62512aef5e45fb1005bd5f7a8bddc69fcb551fb
Fagor <oleg-veber@mail.ru>
2022-11-02 12:07:47 +0700

    Change properties for heroku connection

--- src/main/java/ru/nsu/fit/pak/Budle/repository/UserRepository.java
+++ src/main/java/ru/nsu/fit/pak/Budle/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package ru.nsu.fit.pak.Budle.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import ru.nsu.fit.pak.Budle.dao.User;
 
 import java.util.List;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     List<User> findByPhoneNumber(String phoneNumber);
 }
