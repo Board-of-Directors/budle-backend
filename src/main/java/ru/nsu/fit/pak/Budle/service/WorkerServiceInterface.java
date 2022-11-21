b7afcf56208bf79d078828a521b1c05e2d7ea97e
Fagor <oleg-veber@mail.ru>
2022-11-21 20:53:21 +0700

    style fixes + delete imports

--- src/main/java/ru/nsu/fit/pak/Budle/service/WorkerServiceInterface.java
+++ src/main/java/ru/nsu/fit/pak/Budle/service/WorkerServiceInterface.java
@@ -1,12 +1,7 @@
 package ru.nsu.fit.pak.Budle.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import ru.nsu.fit.pak.Budle.dao.Worker;
 import ru.nsu.fit.pak.Budle.dto.WorkerDto;
-import ru.nsu.fit.pak.Budle.repository.WorkerRepository;
-
-import java.util.List;
 
 @Service
 public interface WorkerServiceInterface {
