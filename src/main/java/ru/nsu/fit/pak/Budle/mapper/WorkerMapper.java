53267dc299114394a2f41f4b53cfca91db6d6183
Fagor <oleg-veber@mail.ru>
2022-11-21 18:48:09 +0700

    worker mapper added

--- src/main/java/ru/nsu/fit/pak/Budle/mapper/WorkerMapper.java
+++ src/main/java/ru/nsu/fit/pak/Budle/mapper/WorkerMapper.java
@@ -1,20 +1,27 @@
 package ru.nsu.fit.pak.Budle.mapper;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import ru.nsu.fit.pak.Budle.dao.Establishment;
 import ru.nsu.fit.pak.Budle.dao.Worker;
 import ru.nsu.fit.pak.Budle.dto.UserDto;
 import ru.nsu.fit.pak.Budle.dto.WorkerDto;
 
 @Component
 public class WorkerMapper {
+    @Autowired
+    EstablishmentMapper establishmentMapper;
+    @Autowired
+    UserMapper userMapper;
+
     public WorkerDto modelToDto(Worker worker) {
         UserMapper userMapper = new UserMapper();
-        EstablishmentMapper establishmentMapper = new EstablishmentMapper();
         WorkerDto dto = new WorkerDto();
         dto.setId(worker.getId());
         dto.setOnWork(worker.getOnWork());
         dto.setWorkerType(worker.getWorkerType());
-        dto.setEstablishments(worker.getEstablishments());
+        dto.setEstablishments(establishmentMapper.modelListToDtoList(worker.getEstablishments()));
+        dto.setUser(userMapper.modelToDto(worker.getUser()));
         return dto;
     }
 }
