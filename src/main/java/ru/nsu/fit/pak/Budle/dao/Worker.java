7ebdaa67869f3587fe8d0ca03a4fc8929eaf2b07
Fagor <oleg-veber@mail.ru>
2022-11-21 17:52:22 +0700

    add workerEntity

--- src/main/java/ru/nsu/fit/pak/Budle/dao/Worker.java
+++ src/main/java/ru/nsu/fit/pak/Budle/dao/Worker.java
@@ -1,4 +1,21 @@
 package ru.nsu.fit.pak.Budle.dao;
 
+import lombok.*;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Getter
+@Setter
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public class Worker {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private Boolean onWork;
+    private String workerType;
+    @OneToOne
+    private User user;
 }
