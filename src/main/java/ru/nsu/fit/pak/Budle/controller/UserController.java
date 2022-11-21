a43d46973e3944cea358d811903020fa8dff321a
Fagor <oleg-veber@mail.ru>
2022-11-21 21:55:17 +0700

    implement custom exception "UserAlreadyExists"

--- src/main/java/ru/nsu/fit/pak/Budle/controller/UserController.java
+++ src/main/java/ru/nsu/fit/pak/Budle/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import ru.nsu.fit.pak.Budle.Exceptions.UserAlreadyExistsException;
 import ru.nsu.fit.pak.Budle.dao.User;
 import ru.nsu.fit.pak.Budle.dto.UserDto;
 import ru.nsu.fit.pak.Budle.service.UserService;
@@ -42,8 +43,10 @@ public class UserController {
             String answer = registered.getFirstName() + " ,you was registered";
             return new ResponseEntity<>(answer, HttpStatus.OK);
 
+        } catch (UserAlreadyExistsException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         } catch (Exception e) {
-            return new ResponseEntity<>("You was not registered", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>("You was not registered because of some problem", HttpStatus.BAD_REQUEST);
         }
     }
 
