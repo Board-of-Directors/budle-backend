2b3a0089ba0779b92f90c40b9d6fad7b9dd8185a
Fagor <oleg-veber@mail.ru>
2022-11-22 10:22:01 +0700

    create SpotRepository

---
+++ src/main/java/ru/nsu/fit/pak/Budle/repository/SpotRepository.java
@@ -0,0 +1,7 @@
+package ru.nsu.fit.pak.Budle.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import ru.nsu.fit.pak.Budle.dao.Spot;
+
+public interface SpotRepository extends CrudRepository<Spot, Long> {
+}
