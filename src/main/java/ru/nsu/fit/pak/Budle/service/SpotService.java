c9335255c73e42bb57ace8e0b027cb2917f96aa2
Fagor <oleg-veber@mail.ru>
2022-11-22 10:25:52 +0700

    implement SpotServiceFindByEstablishment

--- src/main/java/ru/nsu/fit/pak/Budle/service/SpotService.java
+++ src/main/java/ru/nsu/fit/pak/Budle/service/SpotService.java
@@ -1,4 +1,18 @@
 package ru.nsu.fit.pak.Budle.service;
 
-public class SpotService {
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.nsu.fit.pak.Budle.dao.Establishment;
+import ru.nsu.fit.pak.Budle.dao.Spot;
+import ru.nsu.fit.pak.Budle.dto.SpotDto;
+import ru.nsu.fit.pak.Budle.repository.SpotRepository;
+
+import java.util.List;
+
+public class SpotService implements SpotServiceInterface{
+    @Autowired
+    SpotRepository spotRepository;
+    @Override
+    public List<SpotDto> getSpotsByEstablishment(Establishment establishment) {
+        return spotRepository.findByEstablishment(establishment);
+    }
 }
--- src/main/java/ru/nsu/fit/pak/Budle/service/SpotServiceInterface.java
+++ src/main/java/ru/nsu/fit/pak/Budle/service/SpotServiceInterface.java
@@ -2,9 +2,10 @@ package ru.nsu.fit.pak.Budle.service;
 
 import ru.nsu.fit.pak.Budle.dao.Establishment;
 import ru.nsu.fit.pak.Budle.dao.Spot;
+import ru.nsu.fit.pak.Budle.dto.SpotDto;
 
 import java.util.List;
 
 public interface SpotServiceInterface {
-    List<Spot> getSpotsByEstablishment(Establishment establishment);
+    List<SpotDto> getSpotsByEstablishment(Establishment establishment);
 }
