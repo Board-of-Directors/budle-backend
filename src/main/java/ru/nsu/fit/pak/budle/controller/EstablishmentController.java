50c995ad34d8213b792228331c888afd4e6b91f9
Fagor <oleg-veber@mail.ru>
2023-02-06 15:30:27 +0700

    implement getter for categories

--- src/main/java/ru/nsu/fit/pak/budle/controller/EstablishmentController.java
+++ src/main/java/ru/nsu/fit/pak/budle/controller/EstablishmentController.java
@@ -4,22 +4,26 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import ru.nsu.fit.pak.budle.dao.Category;
+import ru.nsu.fit.pak.budle.dto.CategoryDto;
 import ru.nsu.fit.pak.budle.dto.EstablishmentDto;
 import ru.nsu.fit.pak.budle.dto.EstablishmentListDto;
 import ru.nsu.fit.pak.budle.service.EstablishmentServiceImpl;
 
 import javax.validation.Valid;
+import java.util.List;
 
 
 @RestController
+@RequestMapping(value = "/establishment", produces = MediaType.APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor(access = AccessLevel.PUBLIC)
 public class EstablishmentController {
     private final EstablishmentServiceImpl establishmentService;
 
-    
-    @GetMapping("/establishments")
+
+    @GetMapping
     public EstablishmentListDto getEstablishments(@RequestParam(required = false) Category category,
                                                   @RequestParam(required = false) Boolean hasMap,
                                                   @RequestParam(required = false) Boolean hasCardPayment,
@@ -33,9 +37,14 @@ public class EstablishmentController {
         return list;
     }
 
-    @PostMapping("/establishment")
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public void createEstablishment(@Valid @RequestBody EstablishmentDto establishmentDto) {
         establishmentService.createEstablishment(establishmentDto);
+    }
+
+    @GetMapping(value = "/category")
+    public List<CategoryDto> category() {
+        return establishmentService.getCategories();
     }
 
 
