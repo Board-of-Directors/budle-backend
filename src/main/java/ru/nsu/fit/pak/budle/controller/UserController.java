90bc7e527cd818f1497307c023b0deaf7f30340c
Fagor <oleg-veber@mail.ru>
2023-02-06 20:08:50 +0700

    fix controller

--- src/main/java/ru/nsu/fit/pak/budle/controller/UserController.java
+++ src/main/java/ru/nsu/fit/pak/budle/controller/UserController.java
@@ -3,10 +3,7 @@ package ru.nsu.fit.pak.budle.controller;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.nsu.fit.pak.budle.dto.UserDto;
 import ru.nsu.fit.pak.budle.service.UserServiceImpl;
 
@@ -14,21 +11,22 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor(access = AccessLevel.PUBLIC)
+@RequestMapping(value = "user", produces = MediaType.APPLICATION_JSON_VALUE)
 public class UserController {
     private final UserServiceImpl userService;
 
 
-    @GetMapping("/users")
+    @GetMapping
     public List<UserDto> getUsers() {
         return userService.getUsers();
     }
 
-    @PostMapping(value = "/register", consumes = {"application/json"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/register", consumes = MediaType.APPLICATION_JSON_VALUE)
     public Boolean register(@RequestBody UserDto userDto) {
         return userService.registerUser(userDto);
     }
 
-    @PostMapping(value = "/login", consumes = {"application/json"})
+    @PostMapping(value = "/login", consumes = MediaType.APPLICATION_JSON_VALUE)
     public Boolean login(@RequestBody UserDto userDto) {
         return userService.loginUser(userDto);
     }
