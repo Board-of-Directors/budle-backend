5419bbdd8c7277447c973382df4bc0c83d218032
Fagor <oleg-veber@mail.ru>
2023-02-06 15:25:15 +0700

    add sorting

--- src/main/java/ru/nsu/fit/pak/budle/controller/EstablishmentController.java
+++ src/main/java/ru/nsu/fit/pak/budle/controller/EstablishmentController.java
@@ -3,13 +3,14 @@ package ru.nsu.fit.pak.budle.controller;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 import ru.nsu.fit.pak.budle.dao.Category;
 import ru.nsu.fit.pak.budle.dto.EstablishmentDto;
+import ru.nsu.fit.pak.budle.dto.EstablishmentListDto;
 import ru.nsu.fit.pak.budle.service.EstablishmentServiceImpl;
 
 import javax.validation.Valid;
-import java.util.List;
 
 
 @RestController
@@ -17,15 +18,19 @@ import java.util.List;
 public class EstablishmentController {
     private final EstablishmentServiceImpl establishmentService;
 
-
-    // TODO: PAGINATION
+    
     @GetMapping("/establishments")
-    public List<EstablishmentDto> getEstablishments(@RequestParam(required = false) Category category,
-                                                    @RequestParam(required = false) Boolean hasMap,
-                                                    @RequestParam(required = false) Boolean hasCardPayment,
-                                                    @RequestParam(required = false) Integer offset,
-                                                    @RequestParam(required = false) Integer limit) {
-        return establishmentService.getEstablishmentByParams(category, hasMap, hasCardPayment, PageRequest.of(offset, limit));
+    public EstablishmentListDto getEstablishments(@RequestParam(required = false) Category category,
+                                                  @RequestParam(required = false) Boolean hasMap,
+                                                  @RequestParam(required = false) Boolean hasCardPayment,
+                                                  @RequestParam(required = false, defaultValue = "0") Integer offset,
+                                                  @RequestParam(required = false, defaultValue = "100") Integer limit,
+                                                  @RequestParam(required = false, defaultValue = "name") String sortValue) {
+        EstablishmentListDto list = new EstablishmentListDto();
+        list.setEstablishments(establishmentService.getEstablishmentByParams(category, hasMap, hasCardPayment,
+                PageRequest.of(offset, limit, Sort.by(sortValue))));
+        list.setCount(list.getEstablishments().size());
+        return list;
     }
 
     @PostMapping("/establishment")
