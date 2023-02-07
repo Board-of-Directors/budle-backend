6c3ff08f5bb5ca4484b6ca2f8df82184fdce2048
Fagor <oleg-veber@mail.ru>
2023-02-07 11:28:11 +0700

    create handler for notValidArguments

--- src/main/java/ru/nsu/fit/pak/budle/controller/ArticleController.java
+++ src/main/java/ru/nsu/fit/pak/budle/controller/ArticleController.java
@@ -1,15 +1,20 @@
 package ru.nsu.fit.pak.budle.controller;
 
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import ru.nsu.fit.pak.budle.BaseResponse;
 import ru.nsu.fit.pak.budle.exceptions.BaseException;
 import ru.nsu.fit.pak.budle.exceptions.IncorrectDataException;
@@ -17,10 +22,15 @@ import ru.nsu.fit.pak.budle.exceptions.UserAlreadyExistsException;
 import ru.nsu.fit.pak.budle.exceptions.WorkerNotFoundException;
 
 import java.util.LinkedHashMap;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
-public class ArticleController implements ResponseBodyAdvice<Object> {
-    @ExceptionHandler({UserAlreadyExistsException.class, IncorrectDataException.class, WorkerNotFoundException.class})
+public class ArticleController extends ResponseEntityExceptionHandler implements ResponseBodyAdvice<Object> {
+
+
+    @ExceptionHandler({UserAlreadyExistsException.class,
+            IncorrectDataException.class,
+            WorkerNotFoundException.class})
     public <T extends BaseException> ResponseEntity<BaseResponse<Object>> handleException(T e) {
         BaseResponse<Object> response = new BaseResponse<>(e.getMessage(), e.getType());
         return new ResponseEntity<>(response, HttpStatus.OK);
@@ -37,5 +47,15 @@ public class ArticleController implements ResponseBodyAdvice<Object> {
             return body;
         }
         return new BaseResponse<>(body);
+    }
+
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        String message = ex.getBindingResult().getFieldErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.joining(", "));
+        BaseResponse<Object> response = new BaseResponse<>(message, "notValidException");
+        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
     }
 }
