36208596e9f1c237a2563c29da177c7c158d67e6
Fagor <oleg-veber@mail.ru>
2023-02-08 11:50:20 +0700

    implement orderService logic

--- src/main/resources/db/migration/V7__CREATE_ORDERTABLE.sql
+++ src/main/resources/db/migration/V7__CREATE_ORDERTABLE.sql
@@ -1,13 +1,16 @@
 drop table if exists orders cascade;
 
-create table orders(
-                       id          bigserial not null,
-                       user_id     bigserial not null,
-                       spot_id     bigserial not null,
-                       date_time   timestamp,
-                       guest_count int,
-                       primary key (id),
-                       foreign key (user_id) references users (id),
-                       foreign key (spot_id) references spots (id)
+create table orders
+(
+    id               bigserial not null,
+    user_id          bigserial not null,
+    establishment_id bigserial not null,
+    date             date      not null,
+    time             time      not null,
+    status           int4      not null,
+    guest_count      int       not null,
+    primary key (id),
+    foreign key (user_id) references users (id),
+    foreign key (establishment_id) references establishments (id)
 
 )
