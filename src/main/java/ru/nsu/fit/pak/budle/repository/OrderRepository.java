181631583992f3841e35d22a3029d843ba774d98
Fagor <oleg-veber@mail.ru>
2023-02-08 13:38:33 +0700

    fix get orders

--- src/main/java/ru/nsu/fit/pak/budle/repository/OrderRepository.java
+++ src/main/java/ru/nsu/fit/pak/budle/repository/OrderRepository.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Repository;
 import ru.nsu.fit.pak.budle.dao.Order;
 import ru.nsu.fit.pak.budle.dao.User;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepository extends CrudRepository<Order, Long> {
-    void findAllByUser(User user);
+    List<Order> findAllByUser(User user);
 }
--- src/main/java/ru/nsu/fit/pak/budle/service/OrderServiceImpl.java
+++ src/main/java/ru/nsu/fit/pak/budle/service/OrderServiceImpl.java
@@ -1,6 +1,7 @@
 package ru.nsu.fit.pak.budle.service;
 
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import ru.nsu.fit.pak.budle.dao.Order;
 import ru.nsu.fit.pak.budle.dto.OrderDto;
@@ -8,11 +9,15 @@ import ru.nsu.fit.pak.budle.mapper.OrderMapper;
 import ru.nsu.fit.pak.budle.repository.OrderRepository;
 import ru.nsu.fit.pak.budle.repository.UserRepository;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
     private final OrderRepository orderRepository;
     private final OrderMapper orderMapper;
+
+    private final ModelMapper modelMapper;
 
     private final UserRepository userRepository;
 
@@ -22,8 +27,12 @@ public class OrderServiceImpl implements OrderService {
     }
 
     @Override
-    public void getOrders(Long userId) {
-        orderRepository.findAllByUser(userRepository.getReferenceById(userId));
+    public List<OrderDto> getOrders(Long userId) {
+        return orderRepository
+                .findAllByUser(userRepository.getReferenceById(userId))
+                .stream()
+                .map(order -> modelMapper.map(order, OrderDto.class))
+                .toList();
     }
 
     @Override
