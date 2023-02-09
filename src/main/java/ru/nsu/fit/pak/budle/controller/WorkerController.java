683659b1ef56388f3570698c905b641f646f8a85
Fagor <oleg-veber@mail.ru>
2023-02-09 16:02:26 +0700

    implement delete workers

--- src/main/java/ru/nsu/fit/pak/budle/controller/WorkerController.java
+++ src/main/java/ru/nsu/fit/pak/budle/controller/WorkerController.java
@@ -3,12 +3,11 @@ package ru.nsu.fit.pak.budle.controller;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.nsu.fit.pak.budle.dto.WorkerDto;
 import ru.nsu.fit.pak.budle.service.WorkerServiceImpl;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor(access = AccessLevel.PUBLIC)
@@ -17,7 +16,12 @@ public class WorkerController {
     private final WorkerServiceImpl workerService;
 
     @GetMapping
-    public WorkerDto getWorker(@RequestParam Long id) {
-        return workerService.getWorkerById(id);
+    public List<WorkerDto> getWorkers(@RequestParam Long establishmentId) {
+        return workerService.getWorkers(establishmentId);
+    }
+
+    @DeleteMapping
+    public void delete(@RequestParam Long workerId) {
+        workerService.deleteWorker(workerId);
     }
 }
