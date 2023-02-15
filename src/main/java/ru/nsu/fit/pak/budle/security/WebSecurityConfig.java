2227c785da42a5eb8c4c5e35088e79b4baa0655d
Fagor <oleg-veber@mail.ru>
2023-02-15 14:50:27 +0700

    create SecurityService and WebSecurityConfig

---
+++ src/main/java/ru/nsu/fit/pak/budle/security/WebSecurityConfig.java
@@ -0,0 +1,46 @@
+package ru.nsu.fit.pak.budle.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import ru.nsu.fit.pak.budle.service.UserServiceImpl;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Lazy
+    private final UserServiceImpl userService;
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/resources/**", "/registration").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Bean
+    public AuthenticationManager customAuthenticationManager() throws Exception {
+        return authenticationManager();
+    }
+}
---
+++ src/main/java/ru/nsu/fit/pak/budle/service/SecurityService.java
@@ -0,0 +1,7 @@
+package ru.nsu.fit.pak.budle.service;
+
+public interface SecurityService {
+    String findLoggedInUsername();
+
+    void autoLogin(String username, String password);
+}
