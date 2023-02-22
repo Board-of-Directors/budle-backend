2fbbb89f27752600d013f3ed905dea9aa20440ed
Fagor <oleg-veber@mail.ru>
2023-02-22 16:48:50 +0700

    fix EstablishmentAlreadyExistsException

--- src/main/java/ru/nsu/fit/pak/budle/exceptions/EstablishmentAlreadyExistsException.java
+++ src/main/java/ru/nsu/fit/pak/budle/exceptions/EstablishmentAlreadyExistsException.java
@@ -1,7 +1,11 @@
 package ru.nsu.fit.pak.budle.exceptions;
 
 public class EstablishmentAlreadyExistsException extends BaseException {
-    public EstablishmentAlreadyExistsException(String message) {
-        super(message, "EstablishmentAlreadyExists");
+    static final private String MESSAGE_TYPE = "EstablishmentAlreadyExists";
+    
+
+    public EstablishmentAlreadyExistsException(String name, String address) {
+        super("Establishment with name " + name + " and address " + address + " already exists", MESSAGE_TYPE);
+
     }
 }
