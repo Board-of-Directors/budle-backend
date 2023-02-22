30f91792bed4b31c3d08919f76c89a62207ee8ea
Fagor <oleg-veber@mail.ru>
2023-02-22 17:31:34 +0700

    change OrderNotFoundException

--- src/main/java/ru/nsu/fit/pak/budle/exceptions/OrderNotFoundException.java
+++ src/main/java/ru/nsu/fit/pak/budle/exceptions/OrderNotFoundException.java
@@ -1,7 +1,8 @@
 package ru.nsu.fit.pak.budle.exceptions;
 
 public class OrderNotFoundException extends BaseException {
-    public OrderNotFoundException(String message) {
-        super(message, "OrderNotFound");
+    public OrderNotFoundException(Long id) {
+        super(
+                "Заказ с id " + id + " не существует.", "OrderNotFound");
     }
 }
