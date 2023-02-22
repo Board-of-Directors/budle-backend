a02a657b0cc3176c485a67fa7225d51cac1c20ac
Fagor <oleg-veber@mail.ru>
2023-02-22 17:32:48 +0700

    change NotEnoughRightsException

--- src/main/java/ru/nsu/fit/pak/budle/exceptions/NotEnoughRightsException.java
+++ src/main/java/ru/nsu/fit/pak/budle/exceptions/NotEnoughRightsException.java
@@ -2,7 +2,7 @@ package ru.nsu.fit.pak.budle.exceptions;
 
 public class NotEnoughRightsException extends BaseException {
 
-    public NotEnoughRightsException(String message) {
-        super(message, "NotEnoughRights");
+    public NotEnoughRightsException() {
+        super("У вас недостаточно прав для совершения данного действия.", "NotEnoughRights");
     }
 }
