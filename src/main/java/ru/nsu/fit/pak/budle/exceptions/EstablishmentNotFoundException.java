6ea89b71da7af790f6ac6811ea63f210eb60fb3e
Fagor <oleg-veber@mail.ru>
2023-02-22 17:04:19 +0700

    change EstablishmentNotFoundException

--- src/main/java/ru/nsu/fit/pak/budle/exceptions/EstablishmentNotFoundException.java
+++ src/main/java/ru/nsu/fit/pak/budle/exceptions/EstablishmentNotFoundException.java
@@ -1,7 +1,8 @@
 package ru.nsu.fit.pak.budle.exceptions;
 
 public class EstablishmentNotFoundException extends BaseException {
-    public EstablishmentNotFoundException(String s) {
-        super(s, "EstablishmentNotFound");
+
+    public EstablishmentNotFoundException(Long id) {
+        super("Заведения с id " + id + " не существует", "EstablishmentNotFound");
     }
 }
