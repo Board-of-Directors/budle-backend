f05ddfcaa74c81fb3ba0d5efafd1d17055ed4a81
Fagor <oleg-veber@mail.ru>
2023-02-22 09:43:52 +0700

    post develop fix

--- src/main/java/ru/nsu/fit/pak/budle/repository/UserRepository.java
+++ src/main/java/ru/nsu/fit/pak/budle/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package ru.nsu.fit.pak.budle.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 import ru.nsu.fit.pak.budle.dao.User;
 
