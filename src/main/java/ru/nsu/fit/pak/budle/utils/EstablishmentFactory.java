abf15afa6619ae8149f8bde7806790253488635a
Fagor <oleg-veber@mail.ru>
2023-03-09 22:30:53 +0700

    create EstablishmentFactory

---
+++ src/main/java/ru/nsu/fit/pak/budle/utils/EstablishmentFactory.java
@@ -0,0 +1,33 @@
+package ru.nsu.fit.pak.budle.utils;
+
+import ru.nsu.fit.pak.budle.dao.establishment.Establishment;
+import ru.nsu.fit.pak.budle.dao.establishment.hotel.Hotel;
+import ru.nsu.fit.pak.budle.dao.establishment.restaurant.Restaurant;
+import ru.nsu.fit.pak.budle.dto.EstablishmentDto;
+import ru.nsu.fit.pak.budle.dto.HotelDto;
+import ru.nsu.fit.pak.budle.dto.RestaurantDto;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EstablishmentFactory {
+    Map<String, Establishment> entityFactory;
+    Map<String, EstablishmentDto> dtoFactory;
+
+    public EstablishmentFactory() {
+        entityFactory = new HashMap<>();
+        dtoFactory = new HashMap<>();
+        initEntityFactory();
+        initDtoFactory();
+    }
+
+    private void initEntityFactory() {
+        entityFactory.put("hotel", new Hotel());
+        entityFactory.put("restaurant", new Restaurant());
+    }
+
+    private void initDtoFactory() {
+        dtoFactory.put("hotel", new HotelDto());
+        dtoFactory.put("restaurant", new RestaurantDto());
+    }
+}
