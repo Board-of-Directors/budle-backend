05ae736d451a9ef7e0e754b715cfce377435f174
Fagor <oleg-veber@mail.ru>
2023-03-09 22:23:54 +0700

    create parsing inheritances from db

--- src/main/java/ru/nsu/fit/pak/budle/dao/establishment/hotel/Hotel.java
+++ src/main/java/ru/nsu/fit/pak/budle/dao/establishment/hotel/Hotel.java
@@ -5,11 +5,13 @@ import lombok.Setter;
 import ru.nsu.fit.pak.budle.dao.Category;
 import ru.nsu.fit.pak.budle.dao.establishment.Establishment;
 
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
 @Getter
 @Setter
+@DiscriminatorValue(value = "hotel")
 public class Hotel extends Establishment {
     private final Category category = Category.hotel;
     private int starsCount;
--- src/main/java/ru/nsu/fit/pak/budle/dao/establishment/restaurant/Restaurant.java
+++ src/main/java/ru/nsu/fit/pak/budle/dao/establishment/restaurant/Restaurant.java
@@ -5,13 +5,16 @@ import lombok.Setter;
 import ru.nsu.fit.pak.budle.dao.Category;
 import ru.nsu.fit.pak.budle.dao.establishment.Establishment;
 
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 
 @Entity
 @Getter
 @Setter
+@DiscriminatorValue(value = "restaurant")
 public class Restaurant extends Establishment {
     private final Category category = Category.restaurant;
+
     private CuisineCountry cuisineCountry;
 }
