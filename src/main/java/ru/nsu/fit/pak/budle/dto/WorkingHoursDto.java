f730b912dcf01cb4d7d8a5111816ccfca08bff43
Fagor <oleg-veber@mail.ru>
2023-03-10 14:27:49 +0700

    mark working hours dto as data

--- src/main/java/ru/nsu/fit/pak/budle/dto/WorkingHoursDto.java
+++ src/main/java/ru/nsu/fit/pak/budle/dto/WorkingHoursDto.java
@@ -1,7 +1,10 @@
 package ru.nsu.fit.pak.budle.dto;
 
+import lombok.Data;
+
 import java.time.LocalTime;
 
+@Data
 public class WorkingHoursDto {
     private Integer dayOfWeek;
     private LocalTime startTime;
