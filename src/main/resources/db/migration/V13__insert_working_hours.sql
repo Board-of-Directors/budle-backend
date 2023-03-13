4981e6194e0efda43c60a091d4ade15fec29ed22
Fagor <oleg-veber@mail.ru>
2023-03-13 21:05:07 +0700

    repair migration working hours

--- src/main/resources/db/migration/V13__insert_working_hours.sql
+++ src/main/resources/db/migration/V13__insert_working_hours.sql
@@ -3,3 +3,6 @@ values (1, 1, 0, '12:00', '22:00');
 
 insert into working_hours(id, establishment_id, day_of_week, start_time, end_time)
 values (2, 1, 2, '8:00', '22:00');
+
+select nextval('working_hours_id_seq');
+select nextval('working_hours_id_seq');
