d21c3b4741d2ec6c776cf369a30792baca80aded
Fagor <oleg-veber@mail.ru>
2023-03-21 23:09:43 +0700

    getSpotById() and create spot

--- src/main/java/ru/nsu/fit/pak/budle/dao/Spot.java
+++ src/main/java/ru/nsu/fit/pak/budle/dao/Spot.java
@@ -1,15 +1,18 @@
 package ru.nsu.fit.pak.budle.dao;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import ru.nsu.fit.pak.budle.dao.establishment.Establishment;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "spots")
+@NoArgsConstructor
 public class Spot {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,5 +21,11 @@ public class Spot {
     private String status;
     @OneToOne
     private Establishment establishment;
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "spot")
+    private Set<Order> orders;
+
+    public Spot(Establishment establishment) {
+        this.establishment = establishment;
+    }
 
 }
--- src/main/java/ru/nsu/fit/pak/budle/service/SpotServiceImpl.java
+++ src/main/java/ru/nsu/fit/pak/budle/service/SpotServiceImpl.java
@@ -2,6 +2,7 @@ package ru.nsu.fit.pak.budle.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.nsu.fit.pak.budle.dao.Spot;
 import ru.nsu.fit.pak.budle.dao.establishment.Establishment;
 import ru.nsu.fit.pak.budle.dto.SpotDto;
 import ru.nsu.fit.pak.budle.mapper.SpotMapper;
@@ -23,5 +24,14 @@ public class SpotServiceImpl implements SpotService {
     public List<SpotDto> getSpotsByEstablishment(Long establishmentId) {
         Establishment establishment = establishmentRepository.getEstablishmentById(establishmentId);
         return spotMapper.ListModelToListDto(spotRepository.findByEstablishment(establishment));
+    }
+
+    public SpotDto getSpotById(Long spotId) {
+        return spotMapper.modelToDto(spotRepository.findById(spotId)
+                .orElseThrow());
+    }
+
+    public void createSpot(Long establishmentId) {
+        spotRepository.save(new Spot(establishmentRepository.getReferenceById(establishmentId)));
     }
 }
