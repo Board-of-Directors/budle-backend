413e1b4eb5c15ccdffde56dfe731c6abb5e290ba
Fagor <oleg-veber@mail.ru>
2023-03-23 13:19:39 +0700

    added logger to RequestSender

--- src/main/java/ru/nsu/fit/pak/budle/utils/RequestSender.java
+++ src/main/java/ru/nsu/fit/pak/budle/utils/RequestSender.java
@@ -2,6 +2,8 @@ package ru.nsu.fit.pak.budle.utils;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.io.BufferedReader;
@@ -14,32 +16,47 @@ import java.util.Map;
 
 @Component
 public class RequestSender {
-    public Map<String, Object> sendUCaller(String phoneNumber) throws IOException {
-        if (phoneNumber.charAt(0) != '7') {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
+
+    public Map<String, Object> sendUCaller(String phoneNumber) {
+
+        if (!phoneNumber.startsWith("7")) {
             phoneNumber = phoneNumber.substring(1);
         }
-        String url = "https://api.ucaller.ru/v1.0/initCall?" +
+
+        String requestString = "https://api.ucaller.ru/v1.0/initCall?" +
                 "phone=" + phoneNumber +
                 "&voice=" + "true" +
                 "&key=1vvjxSFMby9xJx783gk31AT7UDPEHBdI" +
                 "&service_id=317622";
-        URL obj = new URL(url);
-        HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
-        connection.setRequestMethod("GET");
-        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String inputLine;
-        StringBuilder response = new StringBuilder();
 
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
+        Map<String, Object> map = null;
+
+        try {
+            logger.info("Request to UCaller API was sent");
+            URL request = new URL(requestString);
+            HttpURLConnection connection = (HttpURLConnection) request.openConnection();
+            connection.setRequestMethod("GET");
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            logger.info("Response was received");
+            ObjectMapper objectMapper = new ObjectMapper();
+            map = objectMapper.readValue(
+                    response.toString(), new TypeReference<>() {
+                    });
+            logger.debug("Response was: " + response);
+            connection.disconnect();
+
+        } catch (IOException exception) {
+            logger.warn(exception.getMessage());
         }
-        in.close();
-        ObjectMapper objectMapper = new ObjectMapper();
-        Map<String, Object> map
-                = objectMapper.readValue(response.toString(), new TypeReference<>() {
-        });
-
-        System.out.println(map);
         return map;
     }
 }
