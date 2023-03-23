56d899880c57fb0c5e8743b65d8c60b5268cfd80
Fagor <oleg-veber@mail.ru>
2023-03-23 18:06:09 +0700

    logging spot service

--- src/main/java/ru/nsu/fit/pak/budle/service/SpotServiceImpl.java
+++ src/main/java/ru/nsu/fit/pak/budle/service/SpotServiceImpl.java
@@ -1,6 +1,8 @@
 package ru.nsu.fit.pak.budle.service;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.nsu.fit.pak.budle.dao.Spot;
 import ru.nsu.fit.pak.budle.dao.establishment.Establishment;
@@ -20,20 +22,29 @@ public class SpotServiceImpl implements SpotService {
 
     private final SpotMapper spotMapper;
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Override
     public List<SpotDto> getSpotsByEstablishment(Long establishmentId) {
+        logger.info("Getting spots by establishment");
+        logger.debug("EstablishmentID: " + establishmentId);
         Establishment establishment = establishmentRepository.getEstablishmentById(establishmentId);
         return spotMapper.ListModelToListDto(spotRepository.findByEstablishment(establishment));
     }
 
     @Override
     public SpotDto getSpotById(Long spotId) {
+        logger.info("Getting spot by id");
+        logger.debug("SpotID: " + spotId);
         return spotMapper.modelToDto(spotRepository.findById(spotId)
                 .orElseThrow());
     }
 
     @Override
     public void createSpot(Long localId, Long establishmentId) {
+        logger.info("Saving new spot");
+        logger.debug("LocalID: " + localId);
+        logger.debug("EstablishmentID: " + establishmentId);
         spotRepository.save(
                 new Spot(localId, establishmentRepository.getReferenceById(establishmentId))
         );
