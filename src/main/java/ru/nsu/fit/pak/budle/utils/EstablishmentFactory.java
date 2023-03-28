bb770edb724688480d0f1a9b1e9f4b49aaa22daa
Fagor <oleg-veber@mail.ru>
2023-03-28 19:43:24 +0700

    fix dto

--- src/main/java/ru/nsu/fit/pak/budle/utils/EstablishmentFactory.java
+++ src/main/java/ru/nsu/fit/pak/budle/utils/EstablishmentFactory.java
@@ -1,11 +1,11 @@
 package ru.nsu.fit.pak.budle.utils;
 
 import ru.nsu.fit.pak.budle.dao.establishment.Establishment;
+import ru.nsu.fit.pak.budle.dao.establishment.beauty.Barbershop;
+import ru.nsu.fit.pak.budle.dao.establishment.entertainment.GameClub;
 import ru.nsu.fit.pak.budle.dao.establishment.hotel.Hotel;
 import ru.nsu.fit.pak.budle.dao.establishment.restaurant.Restaurant;
-import ru.nsu.fit.pak.budle.dto.EstablishmentDto;
-import ru.nsu.fit.pak.budle.dto.HotelDto;
-import ru.nsu.fit.pak.budle.dto.RestaurantDto;
+import ru.nsu.fit.pak.budle.dto.*;
 import ru.nsu.fit.pak.budle.exceptions.IncorrectDataException;
 
 import java.util.HashMap;
@@ -25,11 +25,15 @@ public class EstablishmentFactory {
     private void initEntityFactory() {
         entityFactory.put("hotel", new Hotel());
         entityFactory.put("restaurant", new Restaurant());
+        entityFactory.put("game_club", new GameClub());
+        entityFactory.put("barbershop", new Barbershop());
     }
 
     private void initDtoFactory() {
         dtoFactory.put("hotel", new HotelDto());
         dtoFactory.put("restaurant", new RestaurantDto());
+        dtoFactory.put("game_club", new GameClubDto());
+        dtoFactory.put("barbershop", new BarbershopDto());
     }
 
     public Class<? extends Establishment> getEstablishmentEntity(String type) {
