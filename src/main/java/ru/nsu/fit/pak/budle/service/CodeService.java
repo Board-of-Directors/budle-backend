df694f7488ef2844995b7b856e916b3e8044ef57
Fagor <oleg-veber@mail.ru>
2023-04-05 21:21:11 +0700

    CodeService javadocs + delete wrong handled exception

--- src/main/java/ru/nsu/fit/pak/budle/service/CodeService.java
+++ src/main/java/ru/nsu/fit/pak/budle/service/CodeService.java
@@ -1,9 +1,26 @@
 package ru.nsu.fit.pak.budle.service;
 
-import java.io.IOException;
-
+/**
+ * Service, that responsible for verification codes.
+ */
 public interface CodeService {
+    /**
+     * Method, that checking existing of the code in our database.
+     *
+     * @param phoneNumber - with what phone number must be associated code.
+     * @param code        - code, that user provided as his code.
+     * @return true - if data is valid, false - otherwise.
+     */
     boolean checkCode(String phoneNumber, String code);
 
-    boolean generateCode(String phoneNumber) throws IOException;
+
+    /**
+     * Method, that generated code by user request and put it into database.
+     * Also, verified phone number in API service.
+     *
+     * @param phoneNumber - for what number we need to generate code.
+     * @return true - if was success, false - otherwise.
+     */
+
+    boolean generateCode(String phoneNumber);
 }
