292478a35bd3cf7df93aff770a277a5a4cb6338d
Fagor <oleg-veber@mail.ru>
2023-04-05 22:57:53 +0700

    image service javadocs

--- src/main/java/ru/nsu/fit/pak/budle/service/ImageService.java
+++ src/main/java/ru/nsu/fit/pak/budle/service/ImageService.java
@@ -5,7 +5,17 @@ import ru.nsu.fit.pak.budle.dto.PhotoDto;
 
 import java.util.Set;
 
+/**
+ * Service, that responsible for saving images.
+ */
+
 public interface ImageService {
 
+    /**
+     * Saving images for provided establishment.
+     *
+     * @param photos        set of photos those we need to save.
+     * @param establishment which will be associated with those photos.
+     */
     void saveImages(Set<PhotoDto> photos, Establishment establishment);
 }
