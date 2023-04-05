5971308fe350c31d55c87b8aeccfe6af6e540d26
Fagor <oleg-veber@mail.ru>
2023-04-05 12:49:34 +0700

    SpotController javadocs

--- src/main/java/ru/nsu/fit/pak/budle/controller/SpotController.java
+++ src/main/java/ru/nsu/fit/pak/budle/controller/SpotController.java
@@ -9,12 +9,21 @@ import org.springframework.web.bind.annotation.RestController;
 import ru.nsu.fit.pak.budle.dto.SpotDto;
 import ru.nsu.fit.pak.budle.service.SpotServiceImpl;
 
+/**
+ * Class, that represent spot controller.
+ */
 @RestController
 @RequestMapping(value = "/spot", produces = MediaType.APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor
 public class SpotController {
     private final SpotServiceImpl spotService;
 
+    /**
+     * Get request, that used for getting spot dto by id.
+     *
+     * @param spotId id of spot that we need to found.
+     * @return SpotDTO object.
+     */
     @GetMapping()
     public SpotDto get(@RequestParam Long spotId) {
         return spotService.getSpotById(spotId);
