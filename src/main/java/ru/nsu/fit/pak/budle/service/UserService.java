84f3be513b70394589e01f97daa6aa7723f2667f
Fagor <oleg-veber@mail.ru>
2023-04-05 23:20:27 +0700

    user service javadocs

--- src/main/java/ru/nsu/fit/pak/budle/service/UserService.java
+++ src/main/java/ru/nsu/fit/pak/budle/service/UserService.java
@@ -2,8 +2,16 @@ package ru.nsu.fit.pak.budle.service;
 
 import ru.nsu.fit.pak.budle.dto.UserDto;
 
+/**
+ * Service that responsible for users.
+ */
 public interface UserService {
 
+    /**
+     * Function that register new user in our system.
+     *
+     * @param userDto provide information about new user.
+     */
     void registerUser(UserDto userDto);
 
    /* Boolean loginUser(UserDto userDto);
