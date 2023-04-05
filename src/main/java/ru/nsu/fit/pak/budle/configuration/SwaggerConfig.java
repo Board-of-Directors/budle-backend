e6c20b5027b9dbe5d65573aff1bc435a06e89ff9
Fagor <oleg-veber@mail.ru>
2023-04-05 11:13:29 +0700

    SwaggerConfig javadocs

--- src/main/java/ru/nsu/fit/pak/budle/configuration/SwaggerConfig.java
+++ src/main/java/ru/nsu/fit/pak/budle/configuration/SwaggerConfig.java
@@ -7,8 +7,20 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
+
+/**
+ * Swagger class, that configure auto creating API documentation.
+ * You can search for swagger documentation at 80.64.174.33:8080/swagger-ui/#
+ */
 @Configuration
 public class SwaggerConfig {
+
+    /**
+     * Creates instance of Docket class, that configure host URL, controller classes and
+     * API paths.
+     *
+     * @return instance of Docket class.
+     */
     @Bean
     public Docket create() {
         return new Docket(DocumentationType.SWAGGER_2)
