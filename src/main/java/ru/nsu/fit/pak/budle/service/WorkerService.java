ce7d096dba0a4250579c8192328134718c078796
Fagor <oleg-veber@mail.ru>
2023-04-05 23:23:29 +0700

    other services javadocs

--- src/main/java/ru/nsu/fit/pak/budle/service/WorkerService.java
+++ src/main/java/ru/nsu/fit/pak/budle/service/WorkerService.java
@@ -5,10 +5,24 @@ import ru.nsu.fit.pak.budle.dto.WorkerDto;
 
 import java.util.List;
 
+/**
+ * Service that responsible for workers.
+ */
 @Service
 public interface WorkerService {
 
+    /**
+     * Function that gets workers those work in provided establishment.
+     *
+     * @param establishmentId from what establishment we get workers.
+     * @return list of worker dto.
+     */
     List<WorkerDto> getWorkers(Long establishmentId);
 
+    /**
+     * Deleted worker from current establishment.
+     *
+     * @param workerId what worker we need to delete.
+     */
     void deleteWorker(Long workerId);
 }
--- src/main/java/ru/nsu/fit/pak/budle/service/WorkingHoursService.java
+++ src/main/java/ru/nsu/fit/pak/budle/service/WorkingHoursService.java
@@ -7,8 +7,23 @@ import ru.nsu.fit.pak.budle.dto.WorkingHoursDto;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * Service that responsible for working hours.
+ */
 public interface WorkingHoursService {
+    /**
+     * Save provided working hours dto associated with current establishment.
+     *
+     * @param workingHoursDto what we need to save.
+     * @param establishment   with what working hours will be associated.
+     */
     void saveWorkingHours(Set<WorkingHoursDto> workingHoursDto, Establishment establishment);
 
+    /**
+     * Function that computes and return valid booking time for establishment.
+     *
+     * @param establishment for what establishment we need to compute booking times.
+     * @return list of valid times dto.
+     */
     List<ValidTimeDto> getValidBookingHoursByEstablishment(Establishment establishment);
 }
