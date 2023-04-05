be97d1cfe21b4cfb0be67c5679f21549022ebdf5
Fagor <oleg-veber@mail.ru>
2023-04-05 23:19:10 +0700

    spot service javadocs

--- src/main/java/ru/nsu/fit/pak/budle/service/SpotService.java
+++ src/main/java/ru/nsu/fit/pak/budle/service/SpotService.java
@@ -4,10 +4,31 @@ import ru.nsu.fit.pak.budle.dto.SpotDto;
 
 import java.util.List;
 
+/**
+ * Class that responsible for spots.
+ */
 public interface SpotService {
+    /**
+     * Function that gets spots by establishment.
+     *
+     * @param establishmentId which spots we need to find.
+     * @return list of spot dto.
+     */
     List<SpotDto> getSpotsByEstablishment(Long establishmentId);
 
+    /**
+     * Function that gets spot by provided id.
+     *
+     * @param spotId what we need to find.
+     * @return spot dto.
+     */
     SpotDto getSpotById(Long spotId);
 
+    /**
+     * Function that creates spot by local id in establishment and establishment id.
+     *
+     * @param localId         id of spot in current establishment.
+     * @param establishmentId in what establishment we need to create new spot.
+     */
     void createSpot(Long localId, Long establishmentId);
 }
