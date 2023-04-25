e588609585335e457e37976b89057cd7746d38cd
Fagor <oleg-veber@mail.ru>
2023-04-25 23:56:59 +0700

    change migrations

--- src/main/resources/db/migration/V14__establishment_tags.sql
+++ src/main/resources/db/migration/V14__establishment_tags.sql
@@ -3,4 +3,24 @@ create table establishment_tags
     establishment_id bigserial references establishments (id),
     tag_name         varchar not null,
     primary key (establishment_id, tag_name)
-)
+);
+
+
+insert into establishment_tags(establishment_id, tag_name)
+values (1, 'power'),
+       (1, 'wifi'),
+       (1, 'quite'),
+       (1, 'kitchen'),
+       (1, 'dance'),
+       (1, 'television'),
+       (2, 'kitchen'),
+       (3, 'dance'),
+       (4, 'television'),
+       (5, 'power'),
+       (6, 'power'),
+       (7, 'power'),
+       (8, 'kitchen'),
+       (9, 'wifi'),
+       (10, 'quite'),
+       (11, 'quite'),
+       (12, 'dance')
--- src/main/resources/db/migration/V8__INSERT_WORKERS.sql
+++ src/main/resources/db/migration/V8__INSERT_WORKERS.sql
@@ -6,8 +6,6 @@ INSERT INTO workers (user_id, status)
 VALUES (11, 0);
 
 INSERT INTO worker_establishments(worker_id, establishment_id)
-VALUES (1, 1);
-INSERT INTO worker_establishments(worker_id, establishment_id)
-VALUES (2, 2);
-INSERT INTO worker_establishments(worker_id, establishment_id)
-VALUES (3, 2);
+VALUES (1, 1),
+       (2, 2),
+       (3, 2);
