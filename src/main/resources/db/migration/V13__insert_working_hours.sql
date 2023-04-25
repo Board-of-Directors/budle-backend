4bc60a719f6c4a197defa3ab8acd14b525ac3b06
Fagor <oleg-veber@mail.ru>
2023-04-25 23:56:59 +0700

    change migrations

--- src/main/resources/db/migration/V13__insert_working_hours.sql
+++ src/main/resources/db/migration/V13__insert_working_hours.sql
@@ -1,8 +1,12 @@
 insert into working_hours(id, establishment_id, day_of_week, start_time, end_time)
-values (1, 1, 0, '12:00', '22:00');
+values (1, 1, 0, '12:00', '22:00'),
+       (2, 1, 2, '8:00', '22:00'),
+       (3, 1, 1, '8:00', '22:00'),
+       (4, 1, 1, '10:00', '21:00'),
+       (5, 1, 1, '15:00', '23:00');
 
-insert into working_hours(id, establishment_id, day_of_week, start_time, end_time)
-values (2, 1, 2, '8:00', '22:00');
-
+select nextval('working_hours_id_seq');
+select nextval('working_hours_id_seq');
+select nextval('working_hours_id_seq');
 select nextval('working_hours_id_seq');
 select nextval('working_hours_id_seq');
