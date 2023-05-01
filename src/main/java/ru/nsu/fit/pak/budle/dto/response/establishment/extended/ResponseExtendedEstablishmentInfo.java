090f93ce83c9c03d9e248dcd48abada072cb7dfd
Fagor <oleg-veber@mail.ru>
2023-05-01 16:51:13 +0700

    change photo dto set

--- src/main/java/ru/nsu/fit/pak/budle/dto/response/establishment/extended/ResponseExtendedEstablishmentInfo.java
+++ src/main/java/ru/nsu/fit/pak/budle/dto/response/establishment/extended/ResponseExtendedEstablishmentInfo.java
@@ -3,7 +3,7 @@ package ru.nsu.fit.pak.budle.dto.response.establishment.extended;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import ru.nsu.fit.pak.budle.dto.PhotoListDto;
+import ru.nsu.fit.pak.budle.dto.PhotoDto;
 import ru.nsu.fit.pak.budle.dto.WorkingHoursDto;
 import ru.nsu.fit.pak.budle.dto.response.ResponseTagDto;
 import ru.nsu.fit.pak.budle.dto.response.establishment.basic.ResponseBasicEstablishmentInfo;
@@ -21,7 +21,7 @@ import java.util.Set;
 public class ResponseExtendedEstablishmentInfo extends ResponseBasicEstablishmentInfo {
     private Set<ResponseTagDto> tags;
     private String description;
-    private PhotoListDto photos;
+    private Set<PhotoDto> photos;
     private Set<WorkingHoursDto> workingHours;
     private String address;
 }
