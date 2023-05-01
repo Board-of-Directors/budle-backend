f257a5c567cf83bbcc6939431f1247b58c6ac5fe
Fagor <oleg-veber@mail.ru>
2023-05-01 16:09:47 +0700

    deleting code logic

--- src/main/java/ru/nsu/fit/pak/budle/repository/CodeRepository.java
+++ src/main/java/ru/nsu/fit/pak/budle/repository/CodeRepository.java
@@ -3,17 +3,20 @@ package ru.nsu.fit.pak.budle.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.nsu.fit.pak.budle.dao.Code;
 
+import java.util.Optional;
+
 /**
  * Repository, that connects code models with database.
  */
 public interface CodeRepository extends JpaRepository<Code, Long> {
 
     /**
-     * Indicates, if code number exist by phone number and code.
+     * Find code entity, by string code representation and phone number.
      *
      * @param phoneNumber that we search in database.
      * @param code        that we search in database.
-     * @return true - if exists, false - otherwise.
+     * @return optional code entity.
      */
-    boolean existsByPhoneNumberAndCode(String phoneNumber, String code);
+
+    Optional<Code> findByPhoneNumberAndCode(String phoneNumber, String code);
 }
--- src/main/java/ru/nsu/fit/pak/budle/service/CodeServiceImpl.java
+++ src/main/java/ru/nsu/fit/pak/budle/service/CodeServiceImpl.java
@@ -14,6 +14,7 @@ import ru.nsu.fit.pak.budle.repository.UserRepository;
 import ru.nsu.fit.pak.budle.utils.RequestSender;
 
 import java.util.Map;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -27,9 +28,11 @@ public class CodeServiceImpl implements CodeService {
     private final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
 
     @Override
-    public boolean checkCode(String phoneNumber, String code) {
+    public boolean checkCode(String phoneNumber, String codeString) {
         logger.info("Checking code");
-        if (codeRepository.existsByPhoneNumberAndCode(phoneNumber, code)) {
+        Optional<Code> code = codeRepository.findByPhoneNumberAndCode(phoneNumber, codeString);
+        if (code.isPresent()) {
+            codeRepository.delete(code.get());
             logger.debug("Checking code was true");
             return true;
         } else {
