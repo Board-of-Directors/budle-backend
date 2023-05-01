4f4a6def1585bc05c17cd22d317db8b9a74f9df5
Fagor <oleg-veber@mail.ru>
2023-05-01 17:27:18 +0700

    removing establishment changing

--- src/main/java/ru/nsu/fit/pak/budle/dto/response/ResponseUserDto.java
+++ src/main/java/ru/nsu/fit/pak/budle/dto/response/ResponseUserDto.java
@@ -2,9 +2,11 @@ package ru.nsu.fit.pak.budle.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ResponseUserDto {
     private String username;
 }
