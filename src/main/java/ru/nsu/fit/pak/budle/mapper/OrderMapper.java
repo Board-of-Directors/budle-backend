6f166e7ee3a4d4e935e788dafdfb6621801dc995
Fagor <oleg-veber@mail.ru>
2023-05-03 16:46:50 +0700

    fix creating orders with spot

--- src/main/java/ru/nsu/fit/pak/budle/mapper/OrderMapper.java
+++ src/main/java/ru/nsu/fit/pak/budle/mapper/OrderMapper.java
@@ -1,11 +1,12 @@
 package ru.nsu.fit.pak.budle.mapper;
 
+import org.modelmapper.Condition;
 import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.TypeMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ru.nsu.fit.pak.budle.dao.Order;
+import ru.nsu.fit.pak.budle.dao.OrderWithSpot;
 import ru.nsu.fit.pak.budle.dao.Spot;
 import ru.nsu.fit.pak.budle.dto.request.RequestOrderDto;
 import ru.nsu.fit.pak.budle.exceptions.SpotNotFoundException;
@@ -19,11 +20,9 @@ import java.time.temporal.ChronoUnit;
 @Component
 public class OrderMapper {
     private final ModelMapper mapper;
-    private final SpotRepository spotRepository;
 
     @Autowired
     public OrderMapper(ModelMapper mapper, SpotRepository spotRepository) {
-        this.spotRepository = spotRepository;
         final Converter<Long, Spot> converterToSpot = (src) ->
                 (spotRepository.findById(src.getSource())
                         .orElseThrow(() -> new SpotNotFoundException(src.getSource())));
@@ -32,23 +31,9 @@ public class OrderMapper {
         final Converter<Time, Time> converterTime = (src) ->
                 Time.valueOf(src.getSource().toLocalTime().plus(240, ChronoUnit.MINUTES));
 
+        Condition<Long, Spot> notNull = ctx -> ctx.getSource() != null;
 
-        TypeMap<RequestOrderDto, Order> orderTypeMap = mapper
-                .createTypeMap(RequestOrderDto.class, Order.class)
-                .addMapping(RequestOrderDto::getTime, Order::setStartTime)
-                .addMappings(mapping -> mapping.using(converterTime)
-                        .map(RequestOrderDto::getTime,
-                                Order::setEndTime))
-                .addMappings(mapping -> mapping.using(converterDate).map(
-                        RequestOrderDto::getDate,
-                        Order::setDate));
-
-        /* mapper.createTypeMap(RequestOrderDto.class, OrderWithSpot.class)
-                .include(Order.class)
-                .addMappings(mapping -> mapping.using(converterToSpot).map(
-                        RequestOrderDto::getSpotId, OrderWithSpot::setSpot));
-
-         */
+        mapper.createTypeMap(RequestOrderDto.class, OrderWithSpot.class).include(Order.class).addMappings(mapping -> mapping.when(notNull).using(converterToSpot).map(RequestOrderDto::getSpotId, OrderWithSpot::setSpot)).addMapping(RequestOrderDto::getTime, Order::setStartTime).addMappings(mapping -> mapping.using(converterTime).map(RequestOrderDto::getTime, Order::setEndTime)).addMappings(mapping -> mapping.using(converterDate).map(RequestOrderDto::getDate, Order::setDate));
         this.mapper = mapper;
     }
 
