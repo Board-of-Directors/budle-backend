37bd10f09a30f8b965e66c8ea4b2c0504b5d8be3
Fagor <oleg-veber@mail.ru>
2023-05-17 00:26:35 +0700

    no more orders for -1 guest

--- src/main/java/ru/nsu/fit/pak/budle/dto/request/RequestOrderDto.java
+++ src/main/java/ru/nsu/fit/pak/budle/dto/request/RequestOrderDto.java
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.Min;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 @Data
 @AllArgsConstructor
 public class RequestOrderDto {
+    @Min(value = 1, message = "Заказ не может быть на отрицательное количество людей.")
     private Integer guestCount;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate date;
