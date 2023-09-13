31acf8735887668508c2391cc43d286c018ba3ba
Fagorym <oleg-veber@mail.ru>
2023-09-14 01:45:03 +0700

    style fixes

--- src/main/java/ru/nsu/fit/pak/budle/repository/WorkingHoursRepository.java
+++ src/main/java/ru/nsu/fit/pak/budle/repository/WorkingHoursRepository.java
@@ -1,7 +1,6 @@
 package ru.nsu.fit.pak.budle.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ru.nsu.fit.pak.budle.dao.WorkingHours;
 
