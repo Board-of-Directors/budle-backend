35883a946ac7c678f27e04c14e7d9e20da7457c7
Fagorym <oleg-veber@mail.ru>
2023-09-16 11:38:46 +0700

    some fixes

--- src/main/java/ru/nsu/fit/pak/budle/dto/request/RequestGetEstablishmentParameters.java
+++ src/main/java/ru/nsu/fit/pak/budle/dto/request/RequestGetEstablishmentParameters.java
@@ -7,17 +7,18 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 
 @Builder
-public record RequestGetEstablishmentParameters(@Nullable String name,
-                                                @Nullable String category,
-                                                @Nullable Boolean hasMap,
-                                                @Nullable Boolean hasCardPayment,
-                                                @Nullable
-                                                @Min(value = 5, message = "Не может быть меньше 5 рабочих дней.")
-                                                @Max(value = 7, message = "Не может быть больше 7 рабочих дней.")
-                                                Integer workingDayCount,
-                                                @Min(value = 0, message = "Страница не может быть меньше нулевой.") Integer offset,
-                                                @Min(value = 1, message = "Лимит не может быть меньше единицы.") Integer limit,
-                                                String sortValue) {
+public record RequestGetEstablishmentParameters(
+    String name,
+    @Nullable String category,
+    @Nullable Boolean hasMap,
+    @Nullable Boolean hasCardPayment,
+    @Min(value = 5, message = "Не может быть меньше 5 рабочих дней.")
+    @Max(value = 7, message = "Не может быть больше 7 рабочих дней.")
+    Integer workingDayCount,
+    @Min(value = 0, message = "Страница не может быть меньше нулевой.") Integer offset,
+    @Min(value = 1, message = "Лимит не может быть меньше единицы.") Integer limit,
+    String sortValue
+) {
 
     public RequestGetEstablishmentParameters {
         name = name == null ? "" : name;
