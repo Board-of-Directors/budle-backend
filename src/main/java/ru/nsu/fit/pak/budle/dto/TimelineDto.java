9c3126c50851967483dbe3b8222a45e82adda485
Fagor <oleg-veber@mail.ru>
2023-09-23 16:37:26 +0700

    create empty timeline test

--- src/main/java/ru/nsu/fit/pak/budle/dto/TimelineDto.java
+++ src/main/java/ru/nsu/fit/pak/budle/dto/TimelineDto.java
@@ -1,11 +1,17 @@
 package ru.nsu.fit.pak.budle.dto;
 
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
 
 import java.time.LocalTime;
 import java.util.Set;
 
-@Data
+@Getter
+@Setter
+@Accessors(chain = true)
+@EqualsAndHashCode
 public class TimelineDto {
     private LocalTime start;
     private LocalTime end;
