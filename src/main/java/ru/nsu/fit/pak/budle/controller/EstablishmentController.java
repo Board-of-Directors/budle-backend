c99692d04b15e0eff87436a126171ec78cf2a49a
Fagor <oleg-veber@mail.ru>
2023-09-27 09:41:07 +0700

    some javadocs fixes

--- src/main/java/ru/nsu/fit/pak/budle/controller/EstablishmentController.java
+++ src/main/java/ru/nsu/fit/pak/budle/controller/EstablishmentController.java
@@ -4,7 +4,14 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import ru.nsu.fit.pak.budle.dto.EstablishmentListDto;
 import ru.nsu.fit.pak.budle.dto.ValidTimeDto;
 import ru.nsu.fit.pak.budle.dto.request.RequestEstablishmentDto;
@@ -17,7 +24,6 @@ import ru.nsu.fit.pak.budle.service.EstablishmentServiceImpl;
 import ru.nsu.fit.pak.budle.service.OrderService;
 
 import javax.validation.Valid;
-
 import java.util.List;
 
 /**
@@ -32,6 +38,7 @@ public class EstablishmentController {
     private final OrderService orderService;
 
     /**
+     * USER
      * Get requests for establishments.
      * Can filter establishments by fields, also implemented sorting and pagination.
      *
@@ -44,6 +51,7 @@ public class EstablishmentController {
     }
 
     /**
+     * USER
      * Get request for the establishment.
      *
      * @param establishmentId identifier of the establishment
@@ -55,6 +63,7 @@ public class EstablishmentController {
     }
 
     /**
+     * BUSINESS
      * Post request, that creating new establishment with provided fields.
      *
      * @param requestEstablishmentDto - representation of created establishment.
@@ -67,6 +76,7 @@ public class EstablishmentController {
     }
 
     /**
+     * BUSINESS
      * Get request, that returned all existing categories in our application.
      *
      * @return list of existed categories.
@@ -77,6 +87,7 @@ public class EstablishmentController {
     }
 
     /**
+     * BUSINESS
      * Get request for orders, that connected with current establishment.
      *
      * @param establishmentId id of establishment, which orders we need to return.
@@ -93,6 +104,7 @@ public class EstablishmentController {
     }
 
     /**
+     * BUSINESS
      * Get request for tags, that returns all of existing tags in our system.
      *
      * @return list of tags dto.
@@ -108,6 +120,7 @@ public class EstablishmentController {
     }
 
     /**
+     * BUSINESS
      * Put request, that accepted order by worker of establishment.
      *
      * @param establishmentId by what establishment we receive request (will be deleted)
@@ -123,6 +136,7 @@ public class EstablishmentController {
     }
 
     /**
+     * USER
      * Get valid time for booking request of current establishment.
      *
      * @param establishmentId - from what establishment we need to get valid booking time.
@@ -134,6 +148,7 @@ public class EstablishmentController {
     }
 
     /**
+     * BUSINESS
      * Get request, that get all existing in our system spot-tags.
      * WARNING: spot-tags weak-connected with establishment-tags.
      *
@@ -145,6 +160,13 @@ public class EstablishmentController {
         return establishmentService.getSpotTags(establishmentId);
     }
 
+    /**
+     * BUSINESS
+     * Get request, that get all existing variants for current category
+     *
+     * @param category current category
+     * @return variants for categories
+     */
     @GetMapping(value = "/variants")
     public ResponseSubcategoryDto getCategoryVariants(@RequestParam String category) {
         return establishmentService.getCategoryVariants(category);
@@ -152,6 +174,7 @@ public class EstablishmentController {
     }
 
     /**
+     * BUSINESS
      * Put request, that process map of establishment and put it to database.
      *
      * @param establishmentId - in what establishment we add map.
@@ -162,6 +185,14 @@ public class EstablishmentController {
         establishmentService.addMap(establishmentId, map);
     }
 
+    /**
+     * BUSINESS
+     * Put request, that updates establishment with new params.
+     *
+     * @param establishmentId  index of updated establishment
+     * @param establishmentDto new parameters of the establishment
+     */
+
     @PutMapping
     public void update(
         @RequestParam Long establishmentId,
@@ -169,6 +200,13 @@ public class EstablishmentController {
     ) {
         establishmentService.updateEstablishment(establishmentId, establishmentDto);
     }
+
+    /**
+     * BUSINESS
+     * Delete establishment from database.
+     *
+     * @param establishmentId index of deleted establishment.
+     */
 
     @DeleteMapping
     public void delete(@RequestParam Long establishmentId) {
